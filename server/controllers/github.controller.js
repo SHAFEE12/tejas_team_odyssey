ab807185d6af5dfa79b0f6bfbfc7dca83b291944
Shafee Ahmad <shafeeahmad322@gmail.com>
2026-09-18 05:14:33 +0530

    Update GitHub controller

--- server/controllers/github.controller.js
+++ server/controllers/github.controller.js
@@ -1,4 +1,4 @@
-const https = require('https');
+﻿const https = require('https');
 const GitHubProfile = require('../models/GitHubProfile');
 
 /* ──────────────────────────────────────────────────────────────
